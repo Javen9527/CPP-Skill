a580e49cdb328903dabc413430eb64125b6baeb3
Javen9527 <75876605+Javen9527@users.noreply.github.com>
2021-01-05 15:16:56 +0800

    Update All 目录.cpp
    add std::function

--- All 目录.cpp
+++ All 目录.cpp
@@ -1,2 +1,14 @@
-1. std::function<>
+1. std::function
+
+  · 定义： 
+    template<class R, class... Args>
+    class function<R(Args...)>
+    
+  · 描述：
+    它是对可调用实体（函数、函数指针、重载()的类、Lamda表达式)的一种类型安全的封装
   
+  · 用法：
+    int f(int a, int b) {return a + b;}
+    function<int(int, int)> func = f;
+  
+2. 
