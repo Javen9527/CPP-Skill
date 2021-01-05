945e23f007e1e93abbb78979f128a960b7a563ae
Javen9527 <75876605+Javen9527@users.noreply.github.com>
2021-01-05 16:29:04 +0800

    Update All 目录.cpp

--- All 目录.cpp
+++ All 目录.cpp
@@ -11,4 +11,56 @@
     int f(int a, int b) {return a + b;}
     function<int(int, int)> func = f;
   
-2. 
+2. std::optional
+
+  · 描述：
+    想要函数的返回值有多个的做法：
+      ① 作为引用参数传参，函数返回值来标志运行状态
+      ② tuple数据结构，例如：
+        tuple<bool, string, string> f(const string& in)
+        {
+          if(in.size() == 0) return make_tuple(false, "", ""); 
+          return make_tuple(true, "hello", "world");
+        }
+      ③ pair + 结构体，例如：
+        struct out
+        {
+          string out1{""};
+          string out2{""};
+        };
+        pair<bool, out> f(const string& in)
+        {
+          out o;
+          if(in.size() == 0) return {false, o};
+          
+          o.out1 = "hello"; o.out2 = "world";
+          return {true,o);
+        }
+        ④ std::optional
+         
+  · 用法：
+    struct out
+    {
+      string out1{""};
+      string out2{""};
+    };
+    optional<out> f(cosnt  string& in)
+    {
+      out o;
+      if(in.size() == 0) return nullopt;
+      o.out1 = "hello"; o.out2 = "world";
+      return o;
+    }
+        
+  
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
